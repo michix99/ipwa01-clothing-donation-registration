f2f287e9b4e744225e89af81b74f161fa656085e
Michaela Andermann <michaela.andermann@gmx.de>
2023-01-26 09:44:42 +0100

    feat: added form validation

--- src/DonationView/DonationForm/DonationForm.tsx
+++ src/DonationView/DonationForm/DonationForm.tsx
@@ -1,16 +1,46 @@
-import { useEffect, useState } from 'react';
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEventHandler,
+  useEffect,
+  useState,
+} from 'react';
 import { Form, Button, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import Select from 'react-select';
 import './DonationForm.scss';
+import location from '../../assets/officeLocation.json';
+
+interface donationData {
+  crisisArea: number;
+  clothCategories: number[];
+}
+
+interface fromHomeDonationData extends donationData {
+  firstName: string;
+  lastName: string;
+  street: string;
+  houseNumber: string;
+  postcode: string;
+  city: string;
+}
 
 function DonationForm(props: { isCollected: boolean }) {
   const { t, i18n } = useTranslation(['donationForm', 'donationView']);
-  const [selectedOption, setSelectedOption] = useState<Array<number>>([]);
+  const [selectedOptions, setSelectedOptions] = useState<Array<number>>([]);
   const [options, setOptions] = useState<
     Array<{ value: number; label: string }>
   >([]);
-  const [validated, setValidated] = useState(false);
+  const [formValid, setFormValid] = useState(false);
+  const [formData, setFormData] = useState<donationData | fromHomeDonationData>(
+    {
+      crisisArea: 0,
+      clothCategories: [],
+    },
+  );
+  const [multiSelectValid, setMultiSelectValid] = useState(true);
+  const [addressValid, setAddressValid] = useState(true);
+  const [postcodeValid, setPostcodeValid] = useState(true);
 
   useEffect(() => {
     setOptions(
@@ -23,20 +53,42 @@ function DonationForm(props: { isCollected: boolean }) {
     );
   }, [i18n.language]);
 
-  const handleSubmit = (event: {
-    currentTarget: any;
-    preventDefault: () => void;
-    stopPropagation: () => void;
-  }) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  function isFromHomeDonationData(
+    object: donationData,
+  ): object is fromHomeDonationData {
+    return 'firstName' in object;
+  }
+
+  const onCrisisAreaSelectChange: ChangeEventHandler<HTMLSelectElement> = (
+    event,
+  ) => {
+    const name = event.target.name;
+    const selectedIndex = (
+      t('crisisAreas', { returnObjects: true }) as string[]
+    ).findIndex((v) => v === event.target.value);
+    setFormData({ ...formData, [name]: selectedIndex });
+  };
+
+  const onControlChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (props.isCollected && isFromHomeDonationData(formData)) {
+      if (!formData.postcode.startsWith(location.postcode.slice(0, 2))) {
+        setAddressValid(false);
+        return;
+      }
+      setAddressValid(true);
     }
 
-    // TODO: need to check cloth selection here!
-
-    setValidated(true);
+    console.log(formData);
+    setFormValid(true);
   };
 
   return (
@@ -48,13 +100,13 @@ function DonationForm(props: { isCollected: boolean }) {
       </h4>
       <Form
         noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
+        validated={formValid}
+        onSubmit={onSubmit}
         className="form"
       >
         <Form.Group className="mb-3">
           <Form.Label>{t('selectionCrisisArea')}</Form.Label>
-          <Form.Select>
+          <Form.Select name="crisisArea" onChange={onCrisisAreaSelectChange}>
             {(t('crisisAreas', { returnObjects: true }) as string[]).map(
               (area, index) => (
                 <option key={index}>{area}</option>
@@ -66,31 +118,48 @@ function DonationForm(props: { isCollected: boolean }) {
         <Form.Group className="mb-3">
           <Form.Label>{t('selectionCloth')}</Form.Label>
           <Select
+            className={`multi-select ${multiSelectValid ? '' : 'invalid'}`}
             isMulti
             options={options}
             onChange={(event) => {
-              setSelectedOption(event.map((e) => e.value));
+              setSelectedOptions(event.map((e) => e.value));
+              if (!selectedOptions.length) {
+                setMultiSelectValid(false);
+              } else {
+                const oldFormData = { ...formData };
+                formData.clothCategories = selectedOptions;
+                setFormData(oldFormData);
+                setMultiSelectValid(true);
+              }
             }}
             value={options.filter(
               (option: { value: number; label: string }) => {
-                return selectedOption.includes(option.value);
+                return selectedOptions.includes(option.value);
               },
             )}
           />
+          {!multiSelectValid && (
+            <Form.Control.Feedback
+              type="invalid"
+              style={{ display: multiSelectValid ? 'none' : 'block' }}
+            >
+              {t('clothCategoriesInvalid')}
+            </Form.Control.Feedback>
+          )}
         </Form.Group>
 
         {props.isCollected && (
           <>
             <Form.Group className="mb-3">
-              <Form.Text>
-                For collecting the cloth we need your address:
-              </Form.Text>
+              <Form.Text>{t('addressDisclaimer')}</Form.Text>
             </Form.Group>
 
             <Row>
               <Form.Group className="col-md-6 mb-3">
                 <Form.Label>{t('firstNameHeader')}</Form.Label>
                 <Form.Control
+                  name="firstName"
+                  onChange={onControlChange}
                   required
                   placeholder={t('firstNamePlaceholder') ?? undefined}
                 />
@@ -99,6 +168,8 @@ function DonationForm(props: { isCollected: boolean }) {
               <Form.Group className="col-md-6 mb-3">
                 <Form.Label>{t('lastNameHeader')}</Form.Label>
                 <Form.Control
+                  name="lastName"
+                  onChange={onControlChange}
                   required
                   placeholder={t('lastNamePlaceholder') ?? undefined}
                 />
@@ -109,6 +180,8 @@ function DonationForm(props: { isCollected: boolean }) {
               <Form.Group className="col-md-7 mb-3">
                 <Form.Label>{t('addressHeader')}</Form.Label>
                 <Form.Control
+                  name="street"
+                  onChange={onControlChange}
                   required
                   placeholder={t('adressPlaceholder') ?? undefined}
                 />
@@ -117,6 +190,8 @@ function DonationForm(props: { isCollected: boolean }) {
               <Form.Group className="col-md-5 mb-3">
                 <Form.Label>{t('houseNumberHeader')}</Form.Label>
                 <Form.Control
+                  name="houseNumber"
+                  onChange={onControlChange}
                   required
                   placeholder={t('houseNumberPlaceholder') ?? undefined}
                 />
@@ -126,24 +201,49 @@ function DonationForm(props: { isCollected: boolean }) {
             <Form.Group className="mb-3">
               <Form.Label>{t('postcodeHeader')}</Form.Label>
               <Form.Control
-                required
+                name="postcode"
+                onChange={(event) => {
+                  onControlChange(event as ChangeEvent<HTMLInputElement>);
+                  const matchPattern = event.target.value.match(/^([0-9]{5})$/);
+                  setPostcodeValid(matchPattern?.length !== 0);
+                }}
                 placeholder={t('postcodePlaceholder') ?? undefined}
               />
+              {!postcodeValid && (
+                <Form.Control.Feedback
+                  type="invalid"
+                  style={{ display: postcodeValid ? 'none' : 'block' }}
+                >
+                  {t('postcodeInvalid')}
+                </Form.Control.Feedback>
+              )}
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>{t('cityHeader')}</Form.Label>
               <Form.Control
+                name="city"
+                onChange={onControlChange}
                 required
                 placeholder={t('cityPlaceholder') ?? undefined}
               />
             </Form.Group>
-            {/* <Form.Group className="mb-3">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
+
+            {!addressValid && (
+              <Form.Control.Feedback
+                type="invalid"
+                style={{ display: addressValid ? 'none' : 'block' }}
+              >
+                {t('addressInvalid')} <br />
+                <strong>
+                  {location.street} {location.houseNumber} <br />
+                  {location.postcode} {location.city}
+                </strong>
+              </Form.Control.Feedback>
+            )}
           </>
         )}
-        <Button className="submit mb-3" variant="success" type="submit">
+        <Button className="submit mt-3 mb-3" variant="success" type="submit">
           {t('submitButton')}
         </Button>
       </Form>
